7de7f00f562a159cee9c1bce35a250156348ddf5
tonylapoche <terrade.antoine.pro@gmail.com>
2026-04-28 18:52:58 +0200

    first commit

---
+++ test/widget_test.dart
@@ -0,0 +1,23 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pocket_extract/main.dart';
+
+void main() {
+  testWidgets('PocketExtract s affiche correctement', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const PocketExtractApp());
+    await tester.pump();
+
+    expect(find.text('PocketExtract'), findsOneWidget);
+    expect(find.text('Analyser'), findsOneWidget);
+    expect(find.text('Recuperer'), findsOneWidget);
+  });
+}
